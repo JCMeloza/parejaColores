5811efc43d92bef80422f2e3f5df4808d2922857
JuanCarlos <melozajuancarlos@gmail.com>
2025-08-21 18:58:50 +0200

    mejorar la inicialización del juego y agregar funcionalidad de reinicio

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import '../sass/main.scss'
-import Game from './class/game.js';
-import Box from './class/box.js';
+import "../sass/main.scss";
+import Game from "./class/game.js";
+import Box from "./class/box.js";
 
-let rows = parseInt(prompt("Ingrese el número de filas del tablero:"))
-;
-let cols = parseInt(prompt("Ingrese el número de columnas del tablero:"));
+let data = Game.getRowsCols();
 
-let game = new Game(rows, cols, 'game');
-let box = new Box();
+let game = new Game(data.rows, data.cols, "game");
+let resetButton= document.getElementById("reset");
+resetButton.addEventListener("click", () => {
+    Game.resetGame();
+});
